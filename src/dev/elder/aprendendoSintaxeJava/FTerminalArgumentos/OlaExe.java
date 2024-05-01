06e97a666d440f0687810e1c9a007b74152a4df3
DevElder <elderluizmoreira1@gmail.com>
2024-05-01 15:47:40 -0300

    initial CommitV1.5

---
+++ src/dev/elder/aprendendoSintaxeJava/FTerminalArgumentos/OlaExe.java
@@ -0,0 +1,7 @@
+package dev.elder.aprendendoSintaxeJava.FTerminalArgumentos;
+
+public class OlaExe {
+    public static void main(String[] args) {
+        System.out.println("Ola, fui EXEcutado pelo terminal");
+    }
+}
